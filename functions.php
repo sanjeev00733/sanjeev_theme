c7c5bb3727c05d7149dc36c796d11bf60c03d451
Saurabh Shukla <saurabh@yapapaya.com>
2017-03-21 10:09:16 +0530

    Adds PT Sans font

--- functions.php
+++ functions.php
@@ -110,6 +110,8 @@ function _s_scripts() {
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	
+	wp_enqueue_style('_s-gfont-pt-sans', 'https://fonts.googleapis.com/css?family=PT+Sans', array(), '20172103');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
